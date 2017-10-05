0bc1ae587312a125a14c2028b1fb48da59acceb2
Casey Gower <caseyg@charter.net>
2017-10-04 19:57:37 -0500

    General Idea with Errors

--- Secret Number Game/ViewController.swift
+++ Secret Number Game/ViewController.swift
@@ -2,7 +2,6 @@
 //  ViewController.swift
 //  Secret Number Game
 //
-//  Created by Casey Gower on 9/23/17.
 //  Copyright © 2017 Casey Gower. All rights reserved.
 //
 
@@ -19,7 +18,56 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBOutlet var Background: UIView!
+    
+    @IBOutlet weak var secretNumber: UILabel!
+    
+    @IBOutlet weak var numberOfGuesses: UILabel!
+    
+    @IBOutlet weak var lastGuess: UILabel!
+    
+    @IBOutlet weak var currentGuess: UILabel!
+    
+    @IBAction func updateCurrentGuess(_ sender: UISlider) {
+        
+        currentGuess.text = String(format: "%.0f", sender.value)
+            }
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var guessSlider: UISlider!
+    
+    @IBAction func submitGuessPressed(_ sender: UIButton) {
+        
+        switch secretNumber {
+        case secretNumber.text?.hashValue < currentGuess.text?.hashValue :
+            lastGuess = currentGuess
+            int(numberOfGuesses) += 1
+            
+            //background color change
+        
+        case secretNumber.text?.hashValue > currentGuess.text?.hashValue :
+        lastGuess = currentGuess
+        numberOfGuesses += 1
+        //Color change to red
+        case secretNumber = CurrentGuess:
+        //Color to Green!
+        //Show number
+        numberOfGuesses += 1
+    }
+    
+    func loadSecretNumber() {
+        
+        submitButton.isHidden = false
+        
+        guessSlider.isHidden = false
+        
+        secretNumber.text = arc4random().description
+        
+        
+        
+    }
+    
+    
+    
 }
 
+}
